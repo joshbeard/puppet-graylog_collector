07c9bfcb4467f123725929955ec5593830d043f2
Jonas Genannt <jonas.genannt@globalways.net>
2015-11-17 15:34:39 +0100

    added specs to test empty server_url

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,6 +8,24 @@ describe 'graylog_collector' do
           facts.merge({:concat_basedir => '/tmp'})
         end
 
+        context "no server_url with default parameters" do
+          let(:params) {{ }}
+
+          it { expect { catalogue }.to raise_error(Puppet::Error, /does not match/) }
+        end
+
+        context "enable_registration = false" do
+          let(:params) {{ :enable_registration => false }}
+
+          it { is_expected.to contain_class('graylog_collector') }
+          it { is_expected.to contain_class('graylog_collector::install') }
+          it { is_expected.to contain_class('graylog_collector::config') }
+          it { is_expected.to contain_class('graylog_collector::service') }
+
+          it { is_expected.not_to contain_user('root') }
+          it { is_expected.not_to contain_group('root') }
+        end
+
         context "graylog_collector class without any parameters" do
           let(:params) {{ :server_url => 'https://localhost:12900' }}
 
